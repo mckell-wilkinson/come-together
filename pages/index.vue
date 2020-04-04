5f6e2632fd0fa9dd2de348ecfd08b3de135a81b2
Daniel <danielvwilkinson@gmail.com>
2020-04-04 15:58:40 +0100

    Added netlify identity widget

--- pages/index.vue
+++ pages/index.vue
@@ -36,6 +36,15 @@ export default {
     Logo
   }
 }
+
+//Netlify identity widget
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
 
 <style>
