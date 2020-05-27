6d50a5a510466d87f4b71ee220c5a8faa6eed7c5
Daniel <danielvwilkinson@gmail.com>
2020-05-27 23:18:12 +0100

    added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <Header />
+
+  <div class="content">
+    <h1 v-if="error.statusCode === 404">Oooops...<br/>sorry page not found</h1>
+    <h1 v-else>Sorry an error occurred</h1>
+    <nuxt-link to="/">Take me home</nuxt-link>
+  </div>
+
+  <Footer />
+    <sidebar />
+  <sidebarSocial />
+  </div>
+</template>
+
+<script>
+
+import Header from "~/components/Header.vue";
+import Footer from "~/components/Footer.vue";
+import sidebar from "~/components/SidebarMenu";
+import sidebarSocial from "~/components/SidebarSocial";
+
+export default {
+
+      components: {
+    Header,
+    Footer,
+    sidebar,
+    sidebarSocial
+  },
+  props: ['error']
+//   layout: 'blog' // you can set a custom layout for the error page
+}
+</script>
+
+<style scoped>
+
+body {
+    min-height: 100vh;
+}
+
+footer {
+    position: absolute;
+    bottom: 0;
+}
+
+@media only screen and (min-width: 1024px) {
+.content {
+    width: 60%;
+}
+}
+    
+
+
+
+</style>
