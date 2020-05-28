abe7dfd3ca6c3033d4086bac946c0c952c374914
Daniel <danielvwilkinson@gmail.com>
2020-05-28 19:32:52 +0100

    added authors to blog pages

--- pages/blog/_blog.vue
+++ pages/blog/_blog.vue
@@ -12,77 +12,146 @@
         <div class="blog-body" v-html="$md.render(blogPost.body)" />
         <div class="addthis_inline_share_toolbox"></div>
       </article>
+      <div v-if="blogPost.author === 'Dan'" class="author">
+        <div class="img-container">
+          <img
+            src="~/static/img/assets/dan-thumbnail.jpg"
+            alt="Author of this article Dan"
+          />
+        </div>
+        <div class="author-container">
+          <h5>Written by Dan</h5>
+          <p>Developer</p>
+        </div>
+      </div>
+      <div v-else-if="blogPost.author === 'Ellie'" class="author">
+        <div class="img-container">
+          <img
+            src="~/static/img/assets/ellie-thumbnail.jpg"
+            alt="Author of this article Ellie"
+          />
+        </div>
+        <div class="author-container">
+          <h5>Written by Ellie</h5>
+          <p>Visual Branding and Technical Director</p>
+        </div>
+      </div>
+      <div v-else-if="blogPost.author === 'Lanks'" class="author">
+        <div class="img-container">
+          <img
+            src="~/static/img/assets/laura-thumbnail.jpg"
+            alt="Author of this article Lanks"
+          />
+        </div>
+        <div class="author-container">
+          <h5>Written by Lanks</h5>
+          <p>Project Manager</p>
+        </div>
+      </div>
+      <div v-else-if="blogPost.author === 'Roisin'" class="author">
+        <div class="img-container">
+          <img
+            src="~/static/img/assets/roisin-thumbnail.jpg"
+            alt="Author of this article Roisin"
+          />
+        </div>
+        <div class="author-container">
+          <h5>Written by Roisin</h5>
+          <p>Creative and Media Manager</p>
+        </div>
+      </div>
 
+      <!-- SOCIAL SHARE  -->
 
-<!-- SOCIAL SHARE  -->
+      <div class="share-container">
+        <h5>Please share:</h5>
+        <div class="share-buttons">
+          <!-- TWITTER -->
+          <ShareNetwork
+            network="Twitter"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            hashtags="cometogether"
+          >
+            <img
+              src="~/static/img/assets/twitter-blue.svg"
+              alt=""
+              class="share-icon twitter"
+            />
+          </ShareNetwork>
+          <!-- FACEBOOK -->
+          <ShareNetwork
+            network="Facebook"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            :description="`${blogPost.title}...${blogPost.description}`"
+            :quote="`${blogPost.title}...${blogPost.description}`"
+            hashtags="cometogether"
+          >
+            <img
+              src="~/static/img/assets/facebook.png"
+              alt=""
+              class="share-icon facebook"
+            />
+          </ShareNetwork>
 
-<div class="share-container">
-  <h5>Please share:</h5>
-<div class="share-buttons">
+          <!-- PINTEREST  -->
+          <ShareNetwork
+            network="Pinterest"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            :media="`${blogPost.thumbnail}`"
+          >
+            <img
+              src="~/static/img/assets/pinterest.svg"
+              alt=""
+              class="share-icon pinterest"
+            />
+          </ShareNetwork>
 
-      <!-- TWITTER -->
-      <ShareNetwork
-        network="Twitter"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        hashtags="cometogether"
-      >
-       <img src="~/static/img/assets/twitter-blue.svg" alt="" class="share-icon twitter">
-      </ShareNetwork>
-      <!-- FACEBOOK -->
-      <ShareNetwork
-        network="Facebook"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        :description="`${blogPost.title}...${blogPost.description}`"
-        :quote="`${blogPost.title}...${blogPost.description}`"
-        hashtags="cometogether"
-      >
-        <img src="~/static/img/assets/facebook.png" alt="" class="share-icon facebook">
-      </ShareNetwork>
+          <!-- EMAIL  -->
+          <ShareNetwork
+            network="Email"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            :description="`${blogPost.title}...${blogPost.description}`"
+          >
+            <img
+              src="~/static/img/assets/mail.svg"
+              alt=""
+              class="share-icon email"
+            />
+          </ShareNetwork>
 
+          <!-- SMS  -->
+          <ShareNetwork
+            network="SMS"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            :description="`${blogPost.title}...${blogPost.description}`"
+          >
+            <img
+              src="~/static/img/assets/smartphone.svg"
+              alt=""
+              class="share-icon sms"
+            />
+          </ShareNetwork>
 
-      <!-- PINTEREST  -->
-      <ShareNetwork
-        network="Pinterest"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        :media="`${blogPost.thumbnail}`"
-      >
-        <img src="~/static/img/assets/pinterest.svg" alt="" class="share-icon pinterest">
-      </ShareNetwork>
-
-      <!-- EMAIL  -->
-      <ShareNetwork
-        network="Email"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        :description="`${blogPost.title}...${blogPost.description}`"
-      >
-       <img src="~/static/img/assets/mail.svg" alt="" class="share-icon email">
-      </ShareNetwork>
-
-      <!-- SMS  -->
-      <ShareNetwork
-        network="SMS"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        :description="`${blogPost.title}...${blogPost.description}`"
-      >
-        <img src="~/static/img/assets/smartphone.svg" alt="" class="share-icon sms">
-      </ShareNetwork>
-
-      <!-- Whatsapp  -->
-      <ShareNetwork
-        network="WhatsApp"
-        :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
-        :title="`${blogPost.title}...${blogPost.description}`"
-        :description="`${blogPost.title}...${blogPost.description}`"
-      >
-        <img src="~/static/img/assets/whatsapp.svg" alt="" class="share-icon whatsapp">
-      </ShareNetwork>
-</div>
-</div>
+          <!-- Whatsapp  -->
+          <ShareNetwork
+            network="WhatsApp"
+            :url="`https://cometogethercollective.co.uk/${$route.fullPath}`"
+            :title="`${blogPost.title}...${blogPost.description}`"
+            :description="`${blogPost.title}...${blogPost.description}`"
+          >
+            <img
+              src="~/static/img/assets/whatsapp.svg"
+              alt=""
+              class="share-icon whatsapp"
+            />
+          </ShareNetwork>
+        </div>
+      </div>
     </div>
     <SideBarMenu />
     <SideBarSocial />
@@ -135,11 +204,46 @@ img {
   margin-bottom: 1em;
 }
 
+.blog {
+  margin-bottom: 2em;
+}
+.author {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-left: 2vw;
+}
+
+.author-container p,
+.author-container h5 {
+  margin: 0;
+  padding: 0;
+}
+
+.author-container p {
+  font-size: 0.7em;
+  opacity: 0.7;
+}
+
+.author-container {
+  margin: 0 0 0 0.5em;
+}
+
+.author > .img-container {
+  width: 100px;
+}
+
+.author > .img-container > img {
+  width: 100%;
+  border-radius: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 .share-container {
   margin-top: 2em;
   text-align: left;
 }
-
 
 .share-buttons {
   display: flex;
@@ -147,16 +251,17 @@ img {
   justify-content: space-between;
   margin-top: 1em;
   max-width: 700px;
-
 }
-
 
 .share-icon {
   width: 37px;
   cursor: pointer;
 }
 
-.share-icon.facebook, .share-icon.pinterest, .share-icon.sms, .share-icon.email {
+.share-icon.facebook,
+.share-icon.pinterest,
+.share-icon.sms,
+.share-icon.email {
   width: 25px;
 }
 @media only screen and (min-width: 1024px) {
